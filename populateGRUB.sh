bd3f29c61f55f2543fa7d615b988ad60444bc93f
Damiano Ognissanti <ognissanti@hotmail.se>
2023-12-18 19:58:19 +0100

    Changed boot order in GRUB
    Changed boot order in GRUB

--- populateGRUB.sh
+++ populateGRUB.sh
@@ -5,22 +5,22 @@ if [ "$EUID" -ne 0 ]
 fi
 
 mount /dev/nvme0n1p2 /mnt
-rm ./includedir/etc/grub.d/40_custom
-touch ./includedir/etc/grub.d/40_custom
-cat << 'HERE' >> ./includedir/etc/grub.d/40_custom
+rm ./includedir/etc/grub.d/10_custom
+touch ./includedir/etc/grub.d/10_custom
+cat << 'HERE' >> ./includedir/etc/grub.d/10_custom
 #!/bin/sh
 exec tail -n +3 $0
 HERE
 
-for i in /mnt/\@iso/*.iso
-do
-base="${i##*/}"
-cat << EOT >> ./includedir/etc/grub.d/40_custom
+files=(/mnt/\@iso/*.iso)
+for ((i=${#files[@]}-1; i>=0; i--)); do
+base="${files[i]##*/}"
+cat << EOT >> ./includedir/etc/grub.d/10_custom
 menuentry "${base}" --class VoidLinux {
     insmod btrfs
     set iso_file="/@iso/${base}"
 EOT
-cat << 'HERE' >> ./includedir/etc/grub.d/40_custom
+cat << 'HERE' >> ./includedir/etc/grub.d/10_custom
     search --set=iso_partition --no-floppy --file $iso_file
     probe --set=iso_partition_uuid --fs-uuid $iso_partition
     set img_dev="/dev/disk/by-uuid/$iso_partition_uuid"
@@ -34,5 +34,5 @@ HERE
 done
 
 umount /mnt
-cp ./includedir/etc/grub.d/40_custom /etc/grub.d/40_custom
+cp ./includedir/etc/grub.d/10_custom /etc/grub.d/10_custom
 update-grub
